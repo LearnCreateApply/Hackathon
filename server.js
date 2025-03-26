1191b62f2751bedf9f5b8635aa14d1a925a038d8
LearnCreateApply <shivamingulkar1234@gmail.com>
2025-03-26 03:56:01 +0000

    26 march 2025 09:26 am

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 3000
 const path = require('path')
 const fs = require('fs')
+app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'templates/home.html'))
@@ -14,11 +15,14 @@ app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname,'templates/login.html'))
 })
 app.post('/signupsubmit',(req,res)=>{
-    const { Fullname,username, password } = req.body;
-    fs.appendFileSync('signuplog.txt', `FullName:${Fullname} UserName:${username} Password:${password}\n`);  
+    const { Fullname,username, password,phone,location } = req.body;
+    fs.appendFileSync('signuplog.txt', `FullName:${Fullname} UserName:${username} Password:${password} Phone no.:${phone} Location:${location}\n`);  
+    res.render('userdashboard',{Fullname:Fullname,username:username,phone:phone,location:location})
+    // res.sendFile(path.join(__dirname,'view/userdashboard.ejs'))
 })
 app.post('/loginsubmit',(req,res)=>{
     const { username, password } = req.body;
+    res.sendFile(path.join(__dirname,'templates/userdashboard.html'))
     fs.appendFileSync('loginlog.txt', `UserName:${username} Password:${password}\n`);  
 })
 app.listen(port, () => {
